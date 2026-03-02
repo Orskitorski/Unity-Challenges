481e05ccee749f3983417d57bf39b6bdf9bd4f33
Oscar Tafvelin <oscartafvelin@gmail.com>
2026-03-02 14:51:36 +0100

    Fix fire not spreading unless objects move

--- My project/Assets/Scripts/SpreadRadius.cs
+++ My project/Assets/Scripts/SpreadRadius.cs
@@ -5,7 +5,7 @@ public class SpreadRadius : MonoBehaviour
     [SerializeField] Burner burner;
     
     //Gets the colliding object and checks if it is currently burning, if not: start burning-function on object as long as the other object is burning
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerStay2D(Collider2D collision)
     {
         Burner obj = collision.GetComponent<Burner>();
 
