2c8f7c39afe93ebf26a72de1f7914de374ab1d2a
Oscar Tafvelin <oscartafvelin@gmail.com>
2026-03-02 13:59:05 +0100

    added deletion of object + burning animation

--- My project/Assets/Scripts/Burner.cs
+++ My project/Assets/Scripts/Burner.cs
@@ -1,47 +1,58 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.EventSystems;
 using System.Collections;
 
 public class Burner : MonoBehaviour
 {
-
+    [SerializeField] GameObject fireParticles;
     private bool mouseEntered = false;
     public bool burning = false;
-    [SerializeField] GameObject fireParticles;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
+        //If mouse has entered object, player clicks and object is not burning: call burn function
         if (mouseEntered && Input.GetMouseButtonDown(0) && !burning)
         {
             Burn();
         }
     }
 
+    //If mouse enters bounds of object, set mouseEntered to true
     public void OnMouseEnter()
     {
         mouseEntered = true;
         Debug.Log("MouseEntered");
     }
 
+    //If mouse exits bounds of object, set mouseEntered to false
     public void OnMouseExit()
     {
-        mouseEntered=false;
+        mouseEntered = false;
     }
 
+    //Object starts burning
     public void Burn()
     {
         burning = true;
         fireParticles.GetComponent<ParticleSystem>().Play();
-        StartCoroutine(BurnTime(5f));
+        StartCoroutine(BurnTime());
         Debug.Log("Burning");
     }
 
-    private IEnumerator BurnTime(float waitForSeconds)
+    //Plays burning animation. Stops particles and deletes object after about 10 seconds
+    private IEnumerator BurnTime()
     {
-        var renderer = GetComponent<SpriteRenderer>();
-        yield return new WaitForSeconds(waitForSeconds);
-        renderer.color = Color.black;
+        anim.Play("ObjectBurn");
+        yield return new WaitForSeconds(10f);
+        StartCoroutine("Delete");
+        fireParticles.GetComponent<ParticleSystem>().Stop();
+        GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
     }
-
 }
