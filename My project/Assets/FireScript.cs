701fbf936f9a4f53781fcfd56c64bab1f7a2604c
Oscar Tafvelin <oscartafvelin@gmail.com>
2026-03-02 11:36:46 +0100

    Added fire particle effect

---
+++ My project/Assets/FireScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System;
+using Unity.VectorGraphics;
+using UnityEngine.UIElements;
+using System.Numerics;
+
+public class FireScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //GetComponent<ParticleSystem>().shape.scale = new Vector2(transform.parent.GetComponent<SpriteRenderer>().bounds.size.x, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
